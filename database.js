f7094613b46c4ab74adc16fd2d041deda9d0863d
Unyc1124 <aditidubey792@gmail.com>
2026-04-14 12:40:46 +0530

    fixed sqlite issue using better-sqlite3

--- database.js
+++ database.js
@@ -1,21 +1,9 @@
-// // backend/database.js
-
-// const sqlite3 = require("sqlite3").verbose();
-
-// const db = new sqlite3.Database("./content.db", (err) => {
-//   if (err) console.error(err.message);
-//   else console.log("✅ Connected to SQLite DB");
-// });
-
-// module.exports = db;
-
-// backend/database.js
-
 const Database = require("better-sqlite3");
 
+// creates DB file automatically
 const db = new Database("content.db");
 
-// create table
+// create table if not exists
 db.prepare(`
   CREATE TABLE IF NOT EXISTS content (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
